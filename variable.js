32f8d5f18ca0411ad0f2270007f9389de60c0a87
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2024-12-25 21:29:10 +0530

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,49 @@
+// variables in js ( let,const, var)
+//  Variables are containers that store values.
+
+// JavaScript is case sensitive.
+
+
+/* 
+ var:- keyword was used to declare variables in older versions of
+JavaScript but has been mostly replaced by let and const.
+
+ let:-  keyword is used to declare a block-scoped variable, which means it
+is only accessible within the block where it is defined.
+
+const:-  keyword is used to declare a constant, which is a variable that
+cannot be reassigned a new value once it has been initialized.
+*/
+
+var x = 10;
+
+console.log(x);
+
+var y = 15;
+
+var z = x + y;
+console.log(z);
+
+var a = (x - y);
+console.log(a);
+
+// const
+const num = 12;
+//  num=80;           //     it will give error
+console.log(num);
+
+// string 
+var name = "AK";
+var name1 = "ak" + "-47";
+
+console.log(name);
+console.log(name1);
+
+
+
+
+let b = "hacking";
+console.log(b);
+b = "javascript";
+console.log(b);
+
