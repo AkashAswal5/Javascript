3f609fb517ba8b92f4e112d295d508dd7490b616
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2024-12-25 19:57:58 +0530

    Create 1index.js

---
+++ 1index.js
@@ -0,0 +1,19 @@
+console.log("hello");
+let x=10;
+console.log(x);
+
+
+// console.log(5+5);
+
+// console/log(x/2);
+
+/* 
+helllo 
+ */
+
+console.log(x, 5 , "ak");
+
+console.log("Multiplication Table of 10 : ");
+for(let i=1 ; i<=10 ; i++){
+    console.log(x,'X' , i, '=', x*i);
+}
