ce3cde9126f11fafa1d41a12b2f5aad09d049ed6
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2024-12-28 23:53:28 +0530

    Create data_type.js

---
+++ data_type.js
@@ -0,0 +1,41 @@
+
+
+/* primitive data type 
+ Boolean
+ number
+ string
+ Null
+ undefined
+ symbol
+ bigint
+ */
+let num=4;            // number
+console.log(typeof num);
+
+let name="javascript";         // string
+console.log(typeof name);
+
+
+let isbool=true;            // boolean
+console.log(typeof isbool);
+
+let a;       // undefined
+console.log(a);
+
+let b=null;   //Null
+console.log(b);
+
+
+let largeNum=98454512145478154575; // Max number value a number data type can hold
+console.log(largeNum+=1);
+
+let largeBignum=BigInt(457787145451424654454146);
+console.log(largeBignum);
+
+/* passed by reference    or       reference data types 
+object 
+array
+function
+date
+
+*/
