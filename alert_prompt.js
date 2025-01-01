d4a8366bc8e106b404d79fc88f7807d7a4575625
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-01-01 19:32:42 +0530

    Update alert_prompt.js

--- alert_prompt.js
+++ alert_prompt.js
@@ -29,3 +29,18 @@ if (age !== null) {
 else {
     alert("you didn't provide your age. ");
 }
+
+
+
+var num=6;
+
+if(num>10){
+    console.log("the number is greater than 10/");
+}
+else if( num>5 && num<10){
+    console.log("number is greater than 5 and less than or equal to 10.")
+}
+else{
+    console.log("the number id less than or equal to 5.");
+}
+
