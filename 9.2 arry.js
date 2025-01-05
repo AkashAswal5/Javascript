f00a5f93af8f8f325e6542d968b5b613ed6b18c4
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-01-05 22:07:07 +0530

    Update 9.2 arry.js

--- 9.2 arry.js
+++ 9.2 arry.js
@@ -1,1 +1,12 @@
 //   Reverse an Array
+
+function reverseArray(arr) {
+    let reversed = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+    return reversed;
+}
+
+let array = [1, 2, 3, 4, 5];
+console.log(reverseArray(array)); // Output: [5, 4, 3, 2, 1]
