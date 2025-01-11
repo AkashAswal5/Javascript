de77a1c3ff7bb00d437f2a4cc64b3eecdd483ce1
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-01-11 22:25:59 +0530

    Update 9 array.cpp

--- 9 array.cpp
+++ 9 array.cpp
@@ -10,3 +10,14 @@ for (let i = 0; i < numbers.length; i++) {
 }
 
 console.log("The sum of the array elements is:", sum);
+
+
+// Exercise 1: Calculate the sum of all the elements in the array.
+
+//Exercise 2: create an array of numbers and then double each element of array.
+
+let arr = [1, 2, 3, 4, 5];
+for (let i = 0; i < arr.length; i++) {
+      arr[i] = 2 * arr[i];  // Doubles each element
+      console.log(arr[i]); // logs the doubled element
+}
