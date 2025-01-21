a642b3bb9f568938b29cfce047a10505609999f9
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-01-21 22:36:23 +0530

    Update 9.5 array.cpp

--- 9.5 array.cpp
+++ 9.5 array.cpp
@@ -1,1 +1,19 @@
+/* 
+Adding and removing elements:-
+push() , unshift()
+pop(), shift()
+*/
+
+const numbers=[1,2,3,4,6];
+
+// adding elements:-
+numbers.push(5); // adding 5 at the end of the array 
+numbers.unshift(0); // add 0 at the beginning of the array
+
+// removing elements:-
+numbers.pop(); // removing the last element (5)
+numbers.shift(); //Removing the First element (0)
+
+console.log(numbers); // otput: [1,2,3]
+
 
