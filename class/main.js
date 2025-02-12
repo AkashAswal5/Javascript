94bf7d97e74d44155a04c9d4ee3dd980ffd39318
AkashAswal5 <149291029+AkashAswal5@users.noreply.github.com>
2025-02-12 20:44:02 +0530

    Create main.js

---
+++ class/main.js
@@ -0,0 +1,47 @@
+// javascript is dynamically typed because its datatype is decidec in the runtype
+/*
+let var const 
+
+let: block scope
+var: function scope
+const: block scope
+*/
+
+
+// var a=10;
+// console.log(a); //10
+// {
+//     var a=8;
+//     console.log(a);  // 8
+// }
+
+// console.log(a); // 8
+
+
+// let a=10;
+// console.log(a); //10
+// {
+//     let a=8;
+//     console.log(a);  // 8
+// }
+
+// console.log(a); // 10
+
+
+// // hoisting 
+// greet();
+// function greet(){
+//     console.log('good Evening');
+// }
+
+// let a; //TDZ
+// console.log(a);
+// a=10;
+
+
+// datatype: 
+// 1) numbers 
+// 2) String
+// 3) boolean
+//  4) object  
+//  5) undefine
